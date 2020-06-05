1f3c9f959918ebb8bb008ba0c9a6649c5a0eab19
Lee424 <tmdghks424@naver.com>
2020-06-05 16:38:44 +0900

    [Add] Completion Button

--- Guidy/Guidy/AR Drawing/EditingViewController.swift
+++ Guidy/Guidy/AR Drawing/EditingViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol pushViewControllerDelegate {
+    func pushVC(_ controller: UIViewController)
+}
+
 class EditingViewController: UIViewController {
 
     @IBOutlet weak var textView: UITextView!
@@ -15,16 +19,29 @@ class EditingViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var botConstOfOptionView: NSLayoutConstraint!
     @IBOutlet weak var fontSizeSlider: UISlider!
+    @IBOutlet weak var completionButton: UIButton!
     
     let toolBar = UIToolbar()
+    
+    var image = UIImage()
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+    var altitude: Double = 0.0
+    var delegate: pushViewControllerDelegate?
+    
+    
     lazy var optionButton = UIBarButtonItem()
     lazy var setconst = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        imageView.image = image
+        
+        textView.delegate = self
+        textView.frame.size.width = self.view.frame.width * 0.9
         textView.center.x = self.view.center.x
-        textView.center.y = self.view.center.y - 100
+        textView.center.y = self.view.center.y - 200
         imageView.layer.cornerRadius = self.view.frame.width / 10
         optionView.layer.cornerRadius = self.view.frame.width / 20
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
@@ -34,6 +51,13 @@ class EditingViewController: UIViewController {
         textView.tintColor = .white
         
         optionButton.tintColor = .white
+        
+        self.view.bringSubviewToFront(optionView)
+        self.view.bringSubviewToFront(completionButton)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textView.becomeFirstResponder()
     }
     
     @IBAction func dragTextView(_ sender: UIPanGestureRecognizer) {
@@ -48,10 +72,11 @@ class EditingViewController: UIViewController {
     }
     
     @IBAction func changeFontSize(_ sender: UISlider) {
-        self.textView.font = UIFont.systemFont(ofSize: CGFloat(sender.value))
-//        self.textView.frame.size = self.textView.contentSize
-//        self.textView.updateTextFont()
+        self.textView.font = UIFont.boldSystemFont(ofSize: CGFloat(sender.value))
         
+        if textView.frame.size.height < self.view.frame.size.height * 0.8 {
+            adjustTextViewSize(textView)
+        }
     }
     
     @IBAction func changeFontColor(_ sender: RoundButton) {
@@ -62,7 +87,19 @@ class EditingViewController: UIViewController {
         optionButton.tintColor = sender.backgroundColor
     }
     
-    
+    @IBAction func didPressCompletion(_ sender: UIButton) {
+        let image = UIImage.createImageWithTextView(imageView, textView)
+        
+        NetworkManager().saveImage(lat: latitude, lon: longitude, altitude: altitude, img: image)
+        
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "SearchArVC") as! SearchARViewContoller
+        
+        self.dismiss(animated: false) {
+            self.delegate?.pushVC(vc)
+//            self.presentingViewController?.navigationController?.pushViewController(vc, animated: true)
+////            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
     
     @objc func keyboardWillShow(notification: NSNotification) {
 
@@ -115,34 +152,48 @@ class EditingViewController: UIViewController {
         textView.endEditing(true)
     }
     
+    func test() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: imageView.frame.size.width, height: imageView.frame.size.height), false, 2.0)
+            
+        let testView = UIImageView(frame: CGRect(x: 0, y: 0, width: imageView.frame.size.width, height: imageView.frame.size.height))
+        
+        testView.image = imageView.image
+        
+            let testView2 = textView!
+            testView.addSubview(testView2)
+        
+            testView.layer.render(in: UIGraphicsGetCurrentContext()!)
+            let img = UIGraphicsGetImageFromCurrentImageContext()
+            
+//            let currentView = UIView.init(frame: CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height))
+//            let currentImage = UIImageView.init(image: image)
+//            currentImage.frame = CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height)
+//            currentView.addSubview(currentImage)
+//            currentView.addSubview(label)
+        
+            
+//            currentView.layer.render(in: UIGraphicsGetCurrentContext()!)
+//            let img = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return img!
+        }
+}
+
+extension EditingViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        if textView.frame.size.height < self.view.frame.size.height * 0.8 {
+            adjustTextViewSize(textView)
+        }
+    }
     
-}
-
-extension UITextView {
-func updateTextFont() {
-    if (self.text.isEmpty || self.bounds.size.equalTo(CGSize.zero)) {
-        return;
-    }
-
-    let textViewSize = self.frame.size;
-    let fixedWidth = textViewSize.width;
-    let expectSize = self.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat(MAXFLOAT)))
-
-
-    var expectFont = self.font
-    if (expectSize.height > textViewSize.height) {
-
-        while (self.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat(MAXFLOAT))).height > textViewSize.height) {
-            expectFont = self.font!.withSize(self.font!.pointSize - 1)
-            self.font = expectFont
-        }
-    }
-    else {
-        while (self.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat(MAXFLOAT))).height < textViewSize.height) {
-            expectFont = self.font
-            self.font = self.font!.withSize(self.font!.pointSize + 1)
-        }
-        self.font = expectFont
+    func adjustTextViewSize(_ textView: UITextView) {
+        let fixedWidth = textView.frame.size.width
+        textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        var newFrame = textView.frame
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+        textView.frame = newFrame
     }
 }
-}
+
+
